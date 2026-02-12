8b529db7bab9aa02354ffd5afd10c97e7b75161c
ALEX-L1ANG <343293684@tdsb.ca>
2026-02-12 16:19:56 +0000

    update 1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/*
+File Name: Lesson 1.3 - strings and Output
+Author: Alex Liang
+Date Created: Feb 12, 2026
+
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +16,30 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+	System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+	System.out.println("* * * * * *\n *       * \n  *     *  \n   *   *   \n    * *    \n     *     ");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	System.out.println("          '__'\n          (oo)\n  /========//\n / || XX ||\n* ||----||\n   VV    VV\n   ''    ''");
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	String var1 = "Fornite";
+	String var2 = "is awful";
+
+	System.out.println(var1 + " ");
+	System.out.print(var2);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	String var1 = "Don't";
+	String var2 = "@ me";
+	System.out.println(var1 + " " + var2);
 	}
 
 
